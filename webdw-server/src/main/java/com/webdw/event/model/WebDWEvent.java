0c0ed4e618ad2822d50aa46ff991763485121030
liujunsong68922 <liujunsong@aliyun.com>
2020-05-19 09:35:13 +0800

    尝试在webdw上增加事件触发和事件处理器，这部分的逻辑尚未完全想清楚。

---
+++ webdw-server/src/main/java/com/webdw/event/model/WebDWEvent.java
@@ -0,0 +1,16 @@
+package com.webdw.event.model;
+
+public class WebDWEvent {
+	/**
+	 * 插入新数据的事件
+	 */
+	public static final String InsertEvent="insert";
+	/**
+	 * 更新数据的事件
+	 */
+	public static final String UpdateEvent="update";
+	/**
+	 * 删除数据的事件
+	 */
+	public static final String DeleteEvent="delete";
+}
